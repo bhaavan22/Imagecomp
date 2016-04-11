3a90c789060576c1a419e5487ebf87436e6058bd
bhaavan22 <bhaavan22@gmail.com>
2016-04-11 20:31:14 +0530

    first commit

---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo apt-get install jpegoptim optipng pngquant gifsicle exiv2 libjpeg-progs -y
+wget -N https://raw.githubusercontent.com/ryanpcmcquen/image-ultimator/master/imgult; sudo install -m755 imgult /usr/local/bin/; rm imgult
+
+
